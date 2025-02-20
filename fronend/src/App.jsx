1205544e535126bfe9c7c326ef74c61df73f3e33
filin <filinooff@gmail.com>
2025-02-20 04:25:35 +0300

    add module registration

---
+++ fronend/src/App.jsx
@@ -0,0 +1,13 @@
+import Log_Reg_In from './components/Log_Reg_In'
+
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center mt-50">
+      <Log_Reg_In/>
+    </div>
+  )
+}
+
+export default App
